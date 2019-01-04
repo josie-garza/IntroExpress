d49c03b9ccfe7871557a3265817fe9387b6ac431
Josie Garza <jpg7@rice.edu>
2019-01-04 14:23:26 -0500

    simple server

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const http = require('http');
-const server = http;
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.writeHead(200);
+  res.write('<h1>Hello World</h1>');
+  res.end();
+});
 
 server.listen(3000);  // port number
