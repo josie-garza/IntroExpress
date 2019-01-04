a02dd746f905c4809599a89e6a863e3fb2e1d0e1
Josie Garza <jpg7@rice.edu>
2019-01-04 15:29:05 -0500

    added form

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const puppies = require('./puppies');
 const path = require('path');
+const morgan = require('morgan');
+
+// const middleware = () => {
+//   return function(req, res, next) => { //without the first arg runs for every page
+//     console.log(req.method, req.path); //middleware, sits between
+//     next(); // call the next funciton in the chain
+//   }
+// }
+//
+// app.use(middleware);
+app.use(morgan('dev'));
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join()); // bad practice because file system is different on OS
+    res.sendFile(path.join(__dirname, './public/index.html')); // bad practice because file system is different on OS
 });
 
 app.get('/puppies', (req, res) => {
@@ -19,9 +32,20 @@ app.get('/puppies/:id', (req, res) => {
   res.send(puppies[puppyId]);
 });
 
-app.post();
+//users have to create something
+app.post('/puppies', (req, res) => {
+  const age = Number(req.body.age) || 0;
+  const name = req.body.name || 'Toto';
+  puppies.push({
+    name,
+    age
+  })
+  res.send(puppies);
+});
 
 app.listen(3000);
+
+
 // raw HTTP requests
 // const http = require('http');
 // const server = http.createServer();
