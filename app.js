c5d13139867075106605c4ee650b1ce8ec9f4933
Josie Garza <jpg7@rice.edu>
2019-01-04 14:58:02 -0500

    commit

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const http = require('http');
-const server = http.createServer();
+const express = require('express');
+const app = express();
+const puppies = require('./puppies');
+const path = require('path');
 
-server.on('request', (req, res) => {
-  res.writeHead(200);
-  res.write('<h1>Hello World</h1>');
-  res.end();
+app.get('/', (req, res) => {
+    res.sendFile(path.join()); // bad practice because file system is different on OS
 });
 
-server.listen(3000);  // port number
+app.get('/puppies', (req, res) => {
+  // res.send('<h1>Hello Darkness</h1>')
+  res.send(puppies);
+});
+
+// param called id is accessible req.params.id
+app.get('/puppies/:id', (req, res) => {
+  //:id allows what comes after id to be variable
+  const puppyId = Number(req.params.id)-1;
+  res.send(puppies[puppyId]);
+});
+
+app.post();
+
+app.listen(3000);
+// raw HTTP requests
+// const http = require('http');
+// const server = http.createServer();
+//
+// server.on('request', (req, res) => {
+//   res.writeHead(200);
+//   res.write('<h1>Hello World</h1>');
+//   res.end();
+// }); // this can get nasty with multiple pages
+//
+// server.listen(3000);  // port number
